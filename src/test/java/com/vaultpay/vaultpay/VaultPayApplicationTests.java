5174b63770692d0b48d9ec1c8e6e77efd017f0ae
sgurjyot <sgurjyot@gmail.com>
2025-05-08 19:52:05 -0230

    🎉 Initial Spring Boot setup with Docker and PostgreSQL

---
+++ src/test/java/com/vaultpay/vaultpay/VaultPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaultpay.vaultpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
